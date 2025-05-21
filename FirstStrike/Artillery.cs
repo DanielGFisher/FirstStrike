62ca9909c10949065bacd7df495a294db66a5b7f
Daniel Fisher <danielg16fisher@gmail.com>
2025-05-21 10:56:42 +0300

    Artillery Implementation

--- FirstStrike/Artillery.cs
+++ FirstStrike/Artillery.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstStrike
+namespace FirstStrike.Models
 {
-    internal class Artillery
+    public class Artillery : StrikeOption
     {
+        public string BombType;
+        public string EffectiveAgainst;
+        public List<int> StrikeCapacity;
+        public Artillery(string pilot) : base("M109", 40, 100.00, new List<string> { "Buildings", "Vehicles", "Personnel", "Open Area" })
+        {
+            BombType = "Explosive-Shells";
+            EffectiveAgainst = "Open-Space";
+            StrikeCapacity = new List<int> {2, 3};
+        }
+
+        public override void StrikeOperation()
+        {
+            Console.WriteLine($"Drone -- {Name} --\nExecuting Operation:\nTarget struck successfully!");
+            AmmoCapacity--;
+        }
     }
 }
